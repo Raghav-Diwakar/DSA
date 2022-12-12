436d3bc12b8538518fab8830c331349b5c95a3fa
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2022-12-12 14:57:48 +0530

    .

---
+++ Tree/Complete_Tree.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    char data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node *MakeNode(int x)
+{
+    struct Node *p;
+    p = (struct Node *)malloc(sizeof(struct Node));
+    p->data = x;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+}
+// complete tree
+int main()
+{
+    struct Node *root = NULL;
+    root = MakeNode(1);
+    root->left = MakeNode(2);
+    root->right = MakeNode(3);
+    root->right->left = MakeNode(4);
+    root->right->right = MakeNode(5);
+    root->right->left->left = MakeNode(6);
+    root->right->left->left->right = MakeNode(7);
+
+    return 0;
+}
